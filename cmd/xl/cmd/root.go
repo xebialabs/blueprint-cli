da8f368a9a1f22e71fc29b0b1b1cc74db167b61d
Eduard Leito <17474345+eleito@users.noreply.github.com>
2018-07-04 11:52:11 +0200

    Add simple root command

---
+++ cmd/xl/cmd/root.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "xl",
+	Short: "This tool interacts with XL Release and XL Deploy",
+	Long: `This CLI provides a fast and straightforward method for provisioning
+XL Release and XL Deploy with YAML files. The files can include items like
+releases, pipelines, applications and target environments.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.xl.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".xl" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".xl")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ cmd/xl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/xebialabs/xl-cli/cmd/xl/cmd"
+
+func main() {
+	cmd.Execute()
+}
