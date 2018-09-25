064e7083f70a2441c8d8796e156547f8c347cb37
Paul van der Ende <pvanderende@xebialabs.com>
2018-09-25 10:44:19 +0200

    LOVE-120 Show version info

---
+++ cmd/xl/cmd/version.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+	"github.com/xebialabs/xl-cli/pkg/xl"
+	"runtime"
+)
+
+var BuildVersion = "undefined"
+var BuildGitCommit = "undefined"
+var BuildDate = "undefined"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display version info",
+	Long:  `Display version info`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version:                 %s\n", BuildVersion)
+		fmt.Printf("API version XL Deploy:   %s\n", xl.XldApiVersion)
+		fmt.Printf("API version XL Release:  %s\n", xl.XlrApiVersion)
+		fmt.Printf("GO version:              %s\n", runtime.Version())
+		fmt.Printf("Git commit:              %s\n", BuildGitCommit)
+		fmt.Printf("Build date:              %s\n", BuildDate)
+		fmt.Printf("OS/Arch:                 %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
