60dff3e1d1243202411ce91ebe9becd37bbc435d
Alexander Albul <aalbul@xebialabs.com>
2018-10-15 13:39:40 +0200

    LOVE-229 CLI: Add Windows support to XL CLI wrapper

---
+++ cmd/xl/cmd/wrapper/source/xlw.bat
@@ -0,0 +1,29 @@
+@echo off
+@rem ##############################################################################
+@rem ##
+@rem ##  XL Cli wrapper script for Windows
+@rem ##
+@rem ##############################################################################
+
+if "%OS%"=="Windows_NT" setlocal
+
+set PROG_DIR=%~dp0
+
+FOR /F "tokens=1,2 delims==" %%a IN (%PROG_DIR%\\xl\\wrapper\\{{.WrapperConfigName}}) DO (set %%a=%%~b)
+
+set XL_WRAPPER_HOME=%LOCALAPPDATA%\\.xebialabs\\wrapper\\%CLI_VERSION%
+
+IF not exist %XL_WRAPPER_HOME% (mkdir %XL_WRAPPER_HOME%)
+
+set BINARY_NAME=xl.exe
+set WRAPPER_BINARY=%XL_WRAPPER_HOME%\\%BINARY_NAME%
+set TEMP_BINARY_FILE=%WRAPPER_BINARY%.%RANDOM%
+
+if not exist %WRAPPER_BINARY% (
+    call bitsadmin /transfer xlBinaryDownloadJob /download /priority normal %CLI_BASE_URL%/%CLI_VERSION%/windows-amd64/%BINARY_NAME% %TEMP_BINARY_FILE%
+    call ren "%TEMP_BINARY_FILE%" %BINARY_NAME%
+)
+
+call %WRAPPER_BINARY% %*
+
+if "%OS%"=="Windows_NT" endlocal
