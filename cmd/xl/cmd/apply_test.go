04c2a2fdf368796382e91862ddc62a68ea1cf010
Paul van der Ende <pvanderende@xebialabs.com>
2018-11-06 11:59:04 +0100

    LOVE-307 no longer include sub dir xvals

--- cmd/xl/cmd/apply_test.go
+++ cmd/xl/cmd/apply_test.go
@@ -5,12 +5,12 @@ import (
 	"github.com/xebialabs/xl-cli/pkg/xl"
 	"io/ioutil"
 	"os"
-	"github.com/magiconair/properties/assert"
 	"fmt"
 	"net/http"
 	"github.com/spf13/viper"
 	"net/http/httptest"
 	"path/filepath"
+	"github.com/stretchr/testify/assert"
 )
 
 
@@ -128,6 +128,38 @@ spec:
 		assert.Equal(t, xl.TransformToMap(documents[3].Spec)[0]["name"], "App2")
 		assert.Equal(t, documents[3].Metadata["Applications-home"], "Applications/XL")
 	})
+
+
+	t.Run("should list xlvals files from relative directory in alphabetical order", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "")
+		defer os.Remove(dir)
+		check(err)
+
+		_, err1 := os.Create(filepath.Join(dir, "b.xlvals"))
+		check(err1)
+
+		_, err2 := os.Create(filepath.Join(dir, "c.xlvals"))
+		check(err2)
+
+		_, err3 := os.Create(filepath.Join(dir, "a.xlvals"))
+		check(err3)
+
+		subdir, err := ioutil.TempDir(dir, "")
+		check(err)
+
+		_, err4 := os.Create(filepath.Join(subdir, "d.xlvals"))
+		check(err4)
+
+		files, err := listRelativeXlValsFiles(dir)
+		check(err)
+
+		assert.Len(t, files, 3)
+		assert.Equal(t, filepath.Base(files[0]), "a.xlvals")
+		assert.Equal(t, filepath.Base(files[1]), "b.xlvals")
+		assert.Equal(t, filepath.Base(files[2]), "c.xlvals")
+		assert.NotContains(t, files, "d.xlvals")
+	})
+
 }
 
 
--- pkg/xl/util.go
+++ pkg/xl/util.go
@@ -2,34 +2,19 @@ package xl
 
 import (
 	"path/filepath"
-	"os"
-	"sort"
-	"path"
-	"strings"
+	"io/ioutil"
 )
 
 func FindByExtInDirSorted(parentPath string, ext string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(parentPath, func(currentPath string, f os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !f.IsDir() && filepath.Ext(currentPath) == ext {
-			files = append(files, currentPath)
-		}
-		return nil
-	})
+	var res []string
+	files, err := ioutil.ReadDir(parentPath) // sorted by filename
 	if err != nil {
 		return nil, err
 	}
-
-	// sort by filename
-	sort.Slice(files, func(i, j int) bool {
-		_, f1 := path.Split(files[i])
-		_, f2 := path.Split(files[j])
-		return strings.Compare(f1, f2) == 0
-	})
-
-	return files, nil
+	for _, f := range files {
+		if !f.IsDir() && filepath.Ext(f.Name()) == ext {
+			res = append(res, filepath.Join(parentPath, f.Name()))
+		}
+	}
+	return res, nil
 }
-
