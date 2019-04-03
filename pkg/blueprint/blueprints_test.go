bac87ba77cead3886cc14e0f32a4c72f66b0957e
deepu105 <d4udts@gmail.com>
2019-04-03 14:13:55 +0200

    LOVE-706 fix tests

--- pkg/blueprint/blueprints_test.go
+++ pkg/blueprint/blueprints_test.go
@@ -1,19 +1,19 @@
 package blueprint
 
 import (
-    b64 "encoding/base64"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "path"
-    "path/filepath"
-    "strings"
-    "testing"
+	b64 "encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
-    "github.com/xebialabs/xl-cli/pkg/models"
-    "github.com/xebialabs/xl-cli/pkg/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xebialabs/xl-cli/pkg/models"
+	"github.com/xebialabs/xl-cli/pkg/util"
 )
 
 // auxiliary functions
@@ -116,106 +116,106 @@ func TestInstantiateBlueprint(t *testing.T) {
 		require.NotNil(t, err)
 		assert.Equal(t, "parameter [Test] is missing required fields: [type]", err.Error())
 	})
-    t.Run("should create output files for valid test template with answers file", func(t *testing.T) {
-        outFolder := "xebialabs"
-        defer RemoveFiles("xld-*.yml")
-        defer RemoveFiles("xlr-*.yml")
-        defer os.RemoveAll(outFolder)
+	t.Run("should create output files for valid test template with answers file", func(t *testing.T) {
+		outFolder := "xebialabs"
+		defer RemoveFiles("xld-*.yml")
+		defer RemoveFiles("xlr-*.yml")
+		defer os.RemoveAll(outFolder)
 
-        // create blueprint
-        err := InstantiateBlueprint(true, GetTestTemplateDir("answer-input"), getDefaultBlueprintContext(t), outFolder, GetTestTemplateDir("answer-input.yaml"), true, false, false)
-        require.Nil(t, err)
+		// create blueprint
+		err := InstantiateBlueprint(true, GetTestTemplateDir("answer-input"), getDefaultBlueprintContext(t), outFolder, GetTestTemplateDir("answer-input.yaml"), true, false, false)
+		require.Nil(t, err)
 
-        // assertions
-        assert.FileExists(t, "xld-environment.yml")
-        assert.FileExists(t, "xld-infrastructure.yml")
-        assert.FileExists(t, "xlr-pipeline.yml")
-        assert.FileExists(t, path.Join(outFolder, valuesFile))
-        assert.FileExists(t, path.Join(outFolder, secretsFile))
-        assert.FileExists(t, path.Join(outFolder, gitignoreFile))
+		// assertions
+		assert.FileExists(t, "xld-environment.yml")
+		assert.FileExists(t, "xld-infrastructure.yml")
+		assert.FileExists(t, "xlr-pipeline.yml")
+		assert.FileExists(t, path.Join(outFolder, valuesFile))
+		assert.FileExists(t, path.Join(outFolder, secretsFile))
+		assert.FileExists(t, path.Join(outFolder, gitignoreFile))
 
-        // check __test__ directory is not there
-        _, err = os.Stat("__test__")
-        assert.True(t, os.IsNotExist(err))
+		// check __test__ directory is not there
+		_, err = os.Stat("__test__")
+		assert.True(t, os.IsNotExist(err))
 
-        // check encoded string value in env template
-        envTemplateFile := GetFileContent("xld-environment.yml")
-        assert.Contains(t, envTemplateFile, fmt.Sprintf("accessSecret: %s", b64.StdEncoding.EncodeToString([]byte("accesssecret"))))
+		// check encoded string value in env template
+		envTemplateFile := GetFileContent("xld-environment.yml")
+		assert.Contains(t, envTemplateFile, fmt.Sprintf("accessSecret: %s", b64.StdEncoding.EncodeToString([]byte("accesssecret"))))
 
-        // check values file
-        valsFile := GetFileContent(path.Join(outFolder, valuesFile))
-        valueMap := map[string]string{
-            "Test": "testing",
-            "ClientCert": "FshYmQzRUNbYTA4Icc3V7JEgLXMNjcSLY9L1H4XQD79coMBRbbJFtOsp0Yk2btCKCAYLio0S8Jw85W5mgpLkasvCrXO5\\nQJGxFvtQc2tHGLj0kNzM9KyAqbUJRe1l40TqfMdscEaWJimtd4oygqVc6y7zW1Wuj1EcDUvMD8qK8FEWfQgm5ilBIldQ\\n",
-            "AppName": "TestApp",
-            "SuperSecret": "invisible",
-            "AWSRegion": "eu-central-1",
-            "DiskSize": "100",
-            "DiskSizeWithBuffer": "125.1",
-            "ShouldNotBeThere": "",
-        }
-        for k, v := range valueMap {
-            assert.Contains(t, valsFile, fmt.Sprintf("%s = %s", k, v))
-        }
+		// check values file
+		valsFile := GetFileContent(path.Join(outFolder, valuesFile))
+		valueMap := map[string]string{
+			"Test":               "testing",
+			"ClientCert":         "FshYmQzRUNbYTA4Icc3V7JEgLXMNjcSLY9L1H4XQD79coMBRbbJFtOsp0Yk2btCKCAYLio0S8Jw85W5mgpLkasvCrXO5\\nQJGxFvtQc2tHGLj0kNzM9KyAqbUJRe1l40TqfMdscEaWJimtd4oygqVc6y7zW1Wuj1EcDUvMD8qK8FEWfQgm5ilBIldQ\\n",
+			"AppName":            "TestApp",
+			"SuperSecret":        "invisible",
+			"AWSRegion":          "eu-central-1",
+			"DiskSize":           "100",
+			"DiskSizeWithBuffer": "125.1",
+			"ShouldNotBeThere":   "",
+		}
+		for k, v := range valueMap {
+			assert.Contains(t, valsFile, fmt.Sprintf("%s = %s", k, v))
+		}
 
-        // check secrets file
-        secretsFile := GetFileContent(path.Join(outFolder, secretsFile))
-        secretsMap := map[string]string{
-            "AWSAccessKey": "accesskey",
-            "AWSAccessSecret": "accesssecret",
-        }
-        for k, v := range secretsMap {
-            assert.Contains(t, secretsFile, fmt.Sprintf("%s = %s", k, v))
-        }
-    })
-    t.Run("should create output files for valid test template in use defaults as values mode", func(t *testing.T) {
-        outFolder := "xebialabs"
-        defer RemoveFiles("xld-*.yml")
-        defer RemoveFiles("xlr-*.yml")
-        defer os.RemoveAll(outFolder)
+		// check secrets file
+		secretsFile := GetFileContent(path.Join(outFolder, secretsFile))
+		secretsMap := map[string]string{
+			"AWSAccessKey":    "accesskey",
+			"AWSAccessSecret": "accesssecret",
+		}
+		for k, v := range secretsMap {
+			assert.Contains(t, secretsFile, fmt.Sprintf("%s = %s", k, v))
+		}
+	})
+	t.Run("should create output files for valid test template in use defaults as values mode", func(t *testing.T) {
+		outFolder := "xebialabs"
+		defer RemoveFiles("xld-*.yml")
+		defer RemoveFiles("xlr-*.yml")
+		defer os.RemoveAll(outFolder)
 
-        // create blueprint
-        err := InstantiateBlueprint(true, GetTestTemplateDir("defaults-as-values"), getDefaultBlueprintContext(t), outFolder, "", false, true, false)
-        require.Nil(t, err)
+		// create blueprint
+		err := InstantiateBlueprint(true, GetTestTemplateDir("defaults-as-values"), getDefaultBlueprintContext(t), outFolder, "", false, true, false)
+		require.Nil(t, err)
 
-        // assertions
-        assert.FileExists(t, "xld-environment.yml")
-        assert.FileExists(t, "xld-infrastructure.yml")
-        assert.FileExists(t, "xlr-pipeline.yml")
-        assert.FileExists(t, path.Join(outFolder, valuesFile))
-        assert.FileExists(t, path.Join(outFolder, secretsFile))
-        assert.FileExists(t, path.Join(outFolder, gitignoreFile))
+		// assertions
+		assert.FileExists(t, "xld-environment.yml")
+		assert.FileExists(t, "xld-infrastructure.yml")
+		assert.FileExists(t, "xlr-pipeline.yml")
+		assert.FileExists(t, path.Join(outFolder, valuesFile))
+		assert.FileExists(t, path.Join(outFolder, secretsFile))
+		assert.FileExists(t, path.Join(outFolder, gitignoreFile))
 
-        // check __test__ directory is not there
-        _, err = os.Stat("__test__")
-        assert.True(t, os.IsNotExist(err))
+		// check __test__ directory is not there
+		_, err = os.Stat("__test__")
+		assert.True(t, os.IsNotExist(err))
 
-        // check values file
-        valsFile := GetFileContent(path.Join(outFolder, valuesFile))
-        valueMap := map[string]string{
-            "Test": "testing",
-            "ClientCert": "qwertyuiop",
-            "AppName": "TestApp",
-            "SuperSecret": "supersecret",
-            "AWSRegion": "eu-central-1",
-            "DiskSize": "10",
-            "DiskSizeWithBuffer": "125.6",
-            "ShouldNotBeThere": "",
-        }
-        for k, v := range valueMap {
-            assert.Contains(t, valsFile, fmt.Sprintf("%s = %s", k, v))
-        }
+		// check values file
+		valsFile := GetFileContent(path.Join(outFolder, valuesFile))
+		valueMap := map[string]string{
+			"Test":               "testing",
+			"ClientCert":         "this is a multiline\\ntext\\n\\nwith escape chars\\n",
+			"AppName":            "TestApp",
+			"SuperSecret":        "supersecret",
+			"AWSRegion":          "eu-central-1",
+			"DiskSize":           "10",
+			"DiskSizeWithBuffer": "125.6",
+			"ShouldNotBeThere":   "",
+		}
+		for k, v := range valueMap {
+			assert.Contains(t, valsFile, fmt.Sprintf("%s = %s", k, v))
+		}
 
-        // check secrets file
-        secretsFile := GetFileContent(path.Join(outFolder, secretsFile))
-        secretsMap := map[string]string{
-            "AWSAccessKey": "accesskey",
-            "AWSAccessSecret": "accesssecret",
-        }
-        for k, v := range secretsMap {
-            assert.Contains(t, secretsFile, fmt.Sprintf("%s = %s", k, v))
-        }
-    })
+		// check secrets file
+		secretsFile := GetFileContent(path.Join(outFolder, secretsFile))
+		secretsMap := map[string]string{
+			"AWSAccessKey":    "accesskey",
+			"AWSAccessSecret": "accesssecret",
+		}
+		for k, v := range secretsMap {
+			assert.Contains(t, secretsFile, fmt.Sprintf("%s = %s", k, v))
+		}
+	})
 	t.Run("should create output files for valid test template without prompts when no registry is defined", func(t *testing.T) {
 		outFolder := "xebialabs"
 		defer RemoveFiles("xld-*.yml")
@@ -293,15 +293,15 @@ func TestInstantiateBlueprint(t *testing.T) {
 		// Check if only saveInXlVals marked fields are in values.xlvals
 		valuesFileContent := GetFileContent(models.BlueprintOutputDir + string(os.PathSeparator) + valuesFile)
 		assert.Contains(t, valuesFileContent, "Test = testing")
-        assert.Contains(t, GetFileContent(path.Join(outFolder, valuesFile)), `FshYmQzRUNbYTA4Icc3V7JEgLXMNjcSLY9L1H4XQD79coMBRbbJFtOsp0Yk2btCKCAYLio0S8Jw85W5mgpLkasvCrXO5\nQJGxFvtQc2tHGLj0kNzM9KyAqbUJRe1l40TqfMdscEaWJimtd4oygqVc6y7zW1Wuj1EcDUvMD8qK8FEWfQgm5ilBIldQ\nomhDPbq8F84KRsRwCgT05mTrxhBtgqGuCHXcr115iUuUNW7dzzP5iXAgEp4Apa30NHzNsy5TUoIZGDJceO2BmAAmG4HS0cZ\notIXJ2BJEx95SGnqO4kZFoRJzghlZMWs50PkskI5JTM6tHFmtZIdYbo7ZvbA0LP71QtTbSDziqDzXoi5uwxpwaDO95fPYv0\nN1ajgotzn4czgX4hA8gFIipmUUA2AYfgQ5jZQ4I9zO5rxxj80lPWFNOnrHzD1jWZAhLgdpyWldWLt9NbcWegrgLpI\nhRA08PILJnV2z79aTfylL7Y3zJ2urSjr0XIbTWQlWwZ1VXBm13IbRffbku0qjFmSuxDrKFCwGEBtRZ4RnseholT8DA0yDIjPCsfY2jo\nCjljgZHYRoIe4E8WsMt0zzp9G0UP7It6jzJok3yk9Ril48yLthkPvyJ4qoH2PTLx8xBeGBJLKmHT9ojDbWQxOXpml72ati\n4jcxmZfSgDUqMPmTRHPqZ47k6f3XTrPxqIDJ8SzOj09OaKzjSYyZnxIEokm1JotTaqhZa64zptKlbuY0kblSbFAGFFQZnn7RjkU3ZKq872gTDh\nAdteR98sbMdmMGipaxgYbCfuomBEdxldjlApbwDiswJkOQIY0Vypwt95M3LAWha4zACRwrYz7rVqDBJqpo6hFh3V6zBRQR2C6GINUJZq3KWWz\nXAI0ncPo95GDraIFnaStGFHu6R1WC7oopSFS6kgbhJL6noGgMjxbmnPzDA8sXVo1GEtyq79oG2CTHBbrODI9KhsKYy3B0\n8Prpu561H6kDtwIyZqZQXHppVaeFbrGlWAsQpp5su5iHhfFllVaCsDI8kYmmy4JdtOEmPYNL3pF7Uf35X0LIdJKb54czjwBuc2rbbifX9mIn30I8tTgq\n9ldZFjj0SwtTxN1hjYh5pRRTdKZkuwNv6v9L0iPitR6YwuCQaIx1LlymGwfR1Zo6u4gLDCqBYjLz2s1jc7o5dhdmVXmMHKFjWrTaVbanLiwJuNWDQb1e14UikLg\nP4l6RiCx5nNF2wbSQ7uYrvDpYa6ToKysXVUTAPLxG3C4BirrQDaSnTThTzMC7GUAmxKAK3tnBHXEqOIsnYZ3rD92iUr2XI65oFIbIT\nXUrYNapiDWYsPEGTaQTX8L1ZkrFaQTL8wC1Zko8aZFfzqmYbNi5OvJydnWWoaRc0eyvnFmtNh0utLQZEME4DXCU3RxET3q6pwsid8DolT1FZtWBE0V3F0XM\nffWx27IYj63dyTtT4UoJwtTgdtXeHAG4a0AGvbfM9p462qEbV3rMNynLWyzQDc3sN6nI-`)
-        assert.Contains(t, valuesFileContent, "DiskSizeWithBuffer = 125.1")
+		assert.Contains(t, GetFileContent(path.Join(outFolder, valuesFile)), `FshYmQzRUNbYTA4Icc3V7JEgLXMNjcSLY9L1H4XQD79coMBRbbJFtOsp0Yk2btCKCAYLio0S8Jw85W5mgpLkasvCrXO5\nQJGxFvtQc2tHGLj0kNzM9KyAqbUJRe1l40TqfMdscEaWJimtd4oygqVc6y7zW1Wuj1EcDUvMD8qK8FEWfQgm5ilBIldQ\nomhDPbq8F84KRsRwCgT05mTrxhBtgqGuCHXcr115iUuUNW7dzzP5iXAgEp4Apa30NHzNsy5TUoIZGDJceO2BmAAmG4HS0cZ\notIXJ2BJEx95SGnqO4kZFoRJzghlZMWs50PkskI5JTM6tHFmtZIdYbo7ZvbA0LP71QtTbSDziqDzXoi5uwxpwaDO95fPYv0\nN1ajgotzn4czgX4hA8gFIipmUUA2AYfgQ5jZQ4I9zO5rxxj80lPWFNOnrHzD1jWZAhLgdpyWldWLt9NbcWegrgLpI\nhRA08PILJnV2z79aTfylL7Y3zJ2urSjr0XIbTWQlWwZ1VXBm13IbRffbku0qjFmSuxDrKFCwGEBtRZ4RnseholT8DA0yDIjPCsfY2jo\nCjljgZHYRoIe4E8WsMt0zzp9G0UP7It6jzJok3yk9Ril48yLthkPvyJ4qoH2PTLx8xBeGBJLKmHT9ojDbWQxOXpml72ati\n4jcxmZfSgDUqMPmTRHPqZ47k6f3XTrPxqIDJ8SzOj09OaKzjSYyZnxIEokm1JotTaqhZa64zptKlbuY0kblSbFAGFFQZnn7RjkU3ZKq872gTDh\nAdteR98sbMdmMGipaxgYbCfuomBEdxldjlApbwDiswJkOQIY0Vypwt95M3LAWha4zACRwrYz7rVqDBJqpo6hFh3V6zBRQR2C6GINUJZq3KWWz\nXAI0ncPo95GDraIFnaStGFHu6R1WC7oopSFS6kgbhJL6noGgMjxbmnPzDA8sXVo1GEtyq79oG2CTHBbrODI9KhsKYy3B0\n8Prpu561H6kDtwIyZqZQXHppVaeFbrGlWAsQpp5su5iHhfFllVaCsDI8kYmmy4JdtOEmPYNL3pF7Uf35X0LIdJKb54czjwBuc2rbbifX9mIn30I8tTgq\n9ldZFjj0SwtTxN1hjYh5pRRTdKZkuwNv6v9L0iPitR6YwuCQaIx1LlymGwfR1Zo6u4gLDCqBYjLz2s1jc7o5dhdmVXmMHKFjWrTaVbanLiwJuNWDQb1e14UikLg\nP4l6RiCx5nNF2wbSQ7uYrvDpYa6ToKysXVUTAPLxG3C4BirrQDaSnTThTzMC7GUAmxKAK3tnBHXEqOIsnYZ3rD92iUr2XI65oFIbIT\nXUrYNapiDWYsPEGTaQTX8L1ZkrFaQTL8wC1Zko8aZFfzqmYbNi5OvJydnWWoaRc0eyvnFmtNh0utLQZEME4DXCU3RxET3q6pwsid8DolT1FZtWBE0V3F0XM\nffWx27IYj63dyTtT4UoJwtTgdtXeHAG4a0AGvbfM9p462qEbV3rMNynLWyzQDc3sN6nI-`)
+		assert.Contains(t, valuesFileContent, "DiskSizeWithBuffer = 125.1")
 
-        // Check if only secret marked fields are in values.xlvals
-        secretsFileContent := GetFileContent(models.BlueprintOutputDir + string(os.PathSeparator) + secretsFile)
+		// Check if only secret marked fields are in values.xlvals
+		secretsFileContent := GetFileContent(models.BlueprintOutputDir + string(os.PathSeparator) + secretsFile)
 
-        assert.Contains(t, secretsFileContent, "AWSAccessKey = accesskey")
-        assert.Contains(t, secretsFileContent, "AWSAccessSecret = accesssecret")
-        assert.NotContains(t, secretsFileContent, "SuperSecret = invisible")
+		assert.Contains(t, secretsFileContent, "AWSAccessKey = accesskey")
+		assert.Contains(t, secretsFileContent, "AWSAccessSecret = accesssecret")
+		assert.NotContains(t, secretsFileContent, "SuperSecret = invisible")
 
 	})
 }
