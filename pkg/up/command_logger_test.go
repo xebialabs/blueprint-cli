7b0b1ab7a71d2e8e4cbcd853a904a98f3a6079dc
deepu105 <d4udts@gmail.com>
2020-01-30 11:17:53 +0100

    fix test

--- pkg/up/command_logger_test.go
+++ pkg/up/command_logger_test.go
@@ -111,7 +111,7 @@ func Test_getCurrentTask(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := getCurrentTask(tt.eventLog, strings.Index(tt.eventLog, generatedPlan))
+			got := getCurrentTask(tt.eventLog, strings.Index(tt.eventLog, GENERATEDPLAN))
 
 			assert.Equal(t, tt.want, got)
 		})
