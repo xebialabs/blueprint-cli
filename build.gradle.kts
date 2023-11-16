d5ca457c9ae2b64209e9748da7eedb4300156a5b
Venkatesh N <venkatesh.nandakumar@digital.ai>
2023-11-16 11:22:55 +0530

    S-97245 Update golang to 1.21.4 and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ allprojects {
 var goInitialBinary = "go"
 val os = detectOs()
 val arch = detectHostArch()
-val goVersion = "1.19"
+val goVersion = "1.21.4"
 val packagePath = "github.com/xebialabs/blueprint-cli"
 val goRootPath = "${project.rootDir}/.gogradle"
 val goPath = "${goRootPath}/project_gopath"
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/xebialabs/blueprint-cli
 
-go 1.19
+go 1.21.4
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.6
