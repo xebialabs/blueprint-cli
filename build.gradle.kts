ce60be8cd35a0df36b0576e7dd5add473e9a3362
Vedran Pugar <72854396+vpugar-digital@users.noreply.github.com>
2024-05-22 08:21:02 +0200

    Bump version (#44)
    Co-authored-by: Vedran Pugar <vedran.pugar@digital.ai>

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ plugins {
 group = "com.xebialabs.xlclient"
 project.defaultTasks = listOf("build")
 
-val releasedVersion = System.getenv()["RELEASE_EXPLICIT"] ?: "24.1.0-${
+val releasedVersion = System.getenv()["RELEASE_EXPLICIT"] ?: "24.3.0-${
     LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))
 }"
 project.extra.set("releasedVersion", releasedVersion)
