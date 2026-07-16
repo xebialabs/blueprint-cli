e3b3531543ea15facdedc6e97b26877e412d17be
Venkatesh N <venkatesh.nandakumar@digital.ai>
2026-07-16 12:14:22 +0530

    S-135233 Introduced success logging when waiting for resource since success state is not explicitly logged after waiting

--- pkg/cloud/k8s/k8s_resource.go
+++ pkg/cloud/k8s/k8s_resource.go
@@ -400,6 +400,8 @@ func (r Resource) WaitForResourceComplex(timeoutMinutes uint, condition string) 
 				fmt.Sprintf("--timeout=%ds", timeoutMinutes*60),
 				"-n", r.Namespace))
 			if err == nil {
+				elapsed := time.Since(start)
+				util.Info("Resource %s reached condition %s (elapsed: %v)\n", resource, condition, elapsed)
 				return nil
 			} else {
 				util.Verbose("Failed waiting for %s to be %s: %s \n%s\n", resource, condition, err.Error(), log)
@@ -432,6 +434,8 @@ func (r Resource) WaitForResource(timeoutMinutes uint, condition string) error {
 				fmt.Sprintf("--timeout=%ds", timeoutMinutes*60),
 				"-n", r.Namespace))
 			if err == nil {
+				elapsed := time.Since(start)
+				util.Info("Resource %s reached condition %s (elapsed: %v)\n", resource, condition, elapsed)
 				return nil
 			} else {
 				util.Verbose("Failed waiting for %s to be %s: %s \n%s\n", resource, condition, err.Error(), log)
