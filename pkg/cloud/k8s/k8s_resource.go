9307abe75a7c851079c1337909f3793eb9fb40bc
Venkatesh N <venkatesh.nandakumar@digital.ai>
2026-07-22 13:31:15 +0530

    S-135233 Success logging on new line with explicit spinner stop

--- pkg/cloud/k8s/k8s_resource.go
+++ pkg/cloud/k8s/k8s_resource.go
@@ -400,8 +400,9 @@ func (r Resource) WaitForResourceComplex(timeoutMinutes uint, condition string) 
 				fmt.Sprintf("--timeout=%ds", timeoutMinutes*60),
 				"-n", r.Namespace))
 			if err == nil {
+				r.spin.Stop()
 				elapsed := time.Since(start)
-				util.Info("Resource %s reached condition %s (elapsed: %v)\n", resource, condition, elapsed)
+				util.Info("%s reached condition %s in %v\n", resource, condition, elapsed)
 				return nil
 			} else {
 				util.Verbose("Failed waiting for %s to be %s: %s \n%s\n", resource, condition, err.Error(), log)
@@ -434,8 +435,9 @@ func (r Resource) WaitForResource(timeoutMinutes uint, condition string) error {
 				fmt.Sprintf("--timeout=%ds", timeoutMinutes*60),
 				"-n", r.Namespace))
 			if err == nil {
+				r.spin.Stop()
 				elapsed := time.Since(start)
-				util.Info("Resource %s reached condition %s (elapsed: %v)\n", resource, condition, elapsed)
+				util.Info("%s reached condition %s in %v\n", resource, condition, elapsed)
 				return nil
 			} else {
 				util.Verbose("Failed waiting for %s to be %s: %s \n%s\n", resource, condition, err.Error(), log)
